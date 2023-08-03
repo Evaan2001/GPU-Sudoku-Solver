cf9ce0a56328efdd4e31f1d4844510ea0032033d
EAHMED2001 <106691579+EAHMED2001@users.noreply.github.com>
2023-08-02 19:36:11 -0500

    Update sudoku.cu

--- sudoku.cu
+++ sudoku.cu
@@ -15,7 +15,7 @@
 #define GROUP_DIM 3
 
 // The number of boards to pass to the solver at one time
-#define BATCH_SIZE (128*200)
+#define BATCH_SIZE (128 * 200)
 
 /**
  * A board is an array of 81 cells. Each cell is encoded as a 16-bit integer.
@@ -27,15 +27,15 @@
  * sudoku board) but you will need to convert column and row to a single index
  * when accessing the board to propagate constraints.
  */
-typedef struct board {
+typedef struct board
+{
   uint16_t cells[BOARD_DIM * BOARD_DIM];
 } board_t;
 
 // Declare a few functions. Documentation is with the function definition.
-void print_board(board_t* board);
+void print_board(board_t *board);
 __host__ __device__ uint16_t digit_to_cell(int digit);
 __host__ __device__ int cell_to_digit(uint16_t cell);
-
 
 /**
  * The function solve_boards calls this GPU kernel to parallely solve sudoku boards. Each block
@@ -46,8 +46,8 @@ __host__ __device__ int cell_to_digit(uint16_t cell);
  * \param boards      An array of boards to be solved.
  */
 
-
-__global__ void solver_kernel(board_t* boards) {
+__global__ void solver_kernel(board_t *boards)
+{
 
   // Get the x,y coordinate of a cell from thread data
   int x = threadIdx.x;
@@ -60,47 +60,56 @@ __global__ void solver_kernel(board_t* boards) {
   // create another variable to hold a potentially new cell value
   uint16_t new_cell = current_cell;
 
-  int predicate; // predicate for __sync_threads_count(); gets incremented if the new_cell value is different from the original value and so tells a block to run the threads again
-  int val = 0; // stores the value held in a cell
+  int predicate;                              // predicate for __sync_threads_count(); gets incremented if the new_cell value is different from the original value and so tells a block to run the threads again
+  int val = 0;                                // stores the value held in a cell
   int i = 0, j = 0, block_x = 0, block_y = 0; // loop control variables
-  do{
+  do
+  {
     predicate = 0;
 
-    if(cell_to_digit(new_cell) == 0){ // if we are working with a blank cell
+    if (cell_to_digit(new_cell) == 0)
+    { // if we are working with a blank cell
 
       // go through the relevant row
-      for(i=0; i<9; i++){ // i here shuffles through the different columns in a row
-        if(i==y)
-            continue;
-        val = cell_to_digit(boards[blockIdx.x].cells[9*x + i]);
-        if (val != 0) {
+      for (i = 0; i < 9; i++)
+      { // i here shuffles through the different columns in a row
+        if (i == y)
+          continue;
+        val = cell_to_digit(boards[blockIdx.x].cells[9 * x + i]);
+        if (val != 0)
+        {
           /* using idempotence (look at the description of cell encoding in the documentation for digit_to_cell) to narrow down the possible values for the cell */
           new_cell = new_cell & ~(1 << val);
         }
       }
 
       // go through the relevant column
-      for(i=0; i<9; i++){ // i here shuffles through the different rows in a column
-        if(i==x)
-            continue;
-        val = cell_to_digit(boards[blockIdx.x].cells[9*i + y]);
-        if (val != 0) {
+      for (i = 0; i < 9; i++)
+      { // i here shuffles through the different rows in a column
+        if (i == x)
+          continue;
+        val = cell_to_digit(boards[blockIdx.x].cells[9 * i + y]);
+        if (val != 0)
+        {
           // same usage of idempotence as in the case of going through the relevant row
           new_cell = new_cell & ~(1 << val);
         }
       }
 
       // Find out the x,y coordinates of the top-left corner of the relevant block
-      block_x = x - x%3;
-      block_y = y - y%3;
+      block_x = x - x % 3;
+      block_y = y - y % 3;
 
       // Search the block
-      for(i=block_x; i<block_x+3; i++){
-        for(j=block_y; j<block_y+3; j++){
-          if(i==x && j==y)
-              continue;
-          val = cell_to_digit(boards[blockIdx.x].cells[9*i + j]);
-          if (val != 0) {
+      for (i = block_x; i < block_x + 3; i++)
+      {
+        for (j = block_y; j < block_y + 3; j++)
+        {
+          if (i == x && j == y)
+            continue;
+          val = cell_to_digit(boards[blockIdx.x].cells[9 * i + j]);
+          if (val != 0)
+          {
             // same usage of idempotence as in the case of going through the relevant row/column
             new_cell = new_cell & ~(1 << val);
           }
@@ -108,42 +117,39 @@ __global__ void solver_kernel(board_t* boards) {
       }
 
       if (boards[blockIdx.x].cells[index] != new_cell)
-      // if we have a new value for the cell (this happens if another thread solved the value for its cell in a previous run
+        // if we have a new value for the cell (this happens if another thread solved the value for its cell in a previous run
         predicate++; // increment the predicate as a non-zero predicate signals a block to run again
 
       boards[blockIdx.x].cells[index] = new_cell; // update the current cell value
-
     }
-  }while(__syncthreads_count(predicate) != 0); // run the loop while any thread reports making progress
+  } while (__syncthreads_count(predicate) != 0); // run the loop while any thread reports making progress
 }
-
 
 /**
  * Take an array of boards and solve them all. The number of boards will be no
  * more than BATCH_SIZE, but may be less if the total number of input boards
  * is not evenly-divisible by BATCH_SIZE.
  *
- * TODO: Implement this function! You will need to add a GPU kernel, and you
- *       will almost certainly want to write helper functions; that is fine.
- *       However, you should not modify any other functions in this file.
- *
  * \param boards      An array of boards that should be solved.
  * \param num_boards  The numebr of boards in the boards array
  */
-void solve_boards(board_t* boards, size_t num_boards) {
+void solve_boards(board_t *boards, size_t num_boards)
+{
 
-  dim3 threads(9,9); // to configure the layout of threads in a block
+  dim3 threads(9, 9); // to configure the layout of threads in a block
 
-  board_t* gpu_boards; // to store the GPU's copy of the boards to solve
+  board_t *gpu_boards; // to store the GPU's copy of the boards to solve
 
   // malloc space for gpu_boards
-  if(cudaMalloc(&gpu_boards, sizeof(board_t) * num_boards) != cudaSuccess) {
+  if (cudaMalloc(&gpu_boards, sizeof(board_t) * num_boards) != cudaSuccess)
+  {
     fprintf(stderr, "Failed to allocate space for %d boards\n", num_boards);
     exit(2);
   }
 
   // copy over the boards from the CPU to GPU
-  if(cudaMemcpy(gpu_boards, boards, sizeof(board_t) * num_boards, cudaMemcpyHostToDevice) != cudaSuccess) {
+  if (cudaMemcpy(gpu_boards, boards, sizeof(board_t) * num_boards, cudaMemcpyHostToDevice) != cudaSuccess)
+  {
     fprintf(stderr, "Failed to copy boards to the GPU\n");
   }
 
@@ -151,18 +157,19 @@ void solve_boards(board_t* boards, size_t num_boards) {
   solver_kernel<<<num_boards, threads>>>(gpu_boards);
 
   // Wait for the threads to finish
-  if(cudaDeviceSynchronize() != cudaSuccess) {
-      fprintf(stderr, "CUDA Error: %s\n", cudaGetErrorString(cudaPeekAtLastError()));
+  if (cudaDeviceSynchronize() != cudaSuccess)
+  {
+    fprintf(stderr, "CUDA Error: %s\n", cudaGetErrorString(cudaPeekAtLastError()));
   }
 
   // Copy the solved boards from the GPU back to the CPU
-  if(cudaMemcpy(boards, gpu_boards, sizeof(board_t) * num_boards, cudaMemcpyDeviceToHost) != cudaSuccess) {
+  if (cudaMemcpy(boards, gpu_boards, sizeof(board_t) * num_boards, cudaMemcpyDeviceToHost) != cudaSuccess)
+  {
     fprintf(stderr, "Failed to copy solved boards from the GPU\n");
   }
 
   // Free memory malloced for GPU
   cudaFree(gpu_boards);
-
 }
 
 /**
@@ -180,11 +187,15 @@ void solve_boards(board_t* boards, size_t num_boards) {
  * \returns       The encoded form of digit using bits to indicate which values
  *                may appear in this cell.
  */
-__host__ __device__ uint16_t digit_to_cell(int digit) {
-  if (digit == 0) {
+__host__ __device__ uint16_t digit_to_cell(int digit)
+{
+  if (digit == 0)
+  {
     // A zero indicates a blank cell. Numbers 1-9 are possible, so set bits 1-9.
     return 0x3FE;
-  } else {
+  }
+  else
+  {
     // Otherwise we have a fixed value. Set the corresponding bit in the board.
     return 1 << digit;
   }
@@ -203,14 +214,15 @@ __host__ __device__ uint16_t digit_to_cell(int digit) {
  * \returns     The value that must appear in the cell if there is only one
  *              possibility, or zero otherwise.
  */
-__host__ __device__ int cell_to_digit(uint16_t cell) {
-  // Get the index of the least-significant bit in this cell's value
-  #if defined(__CUDA_ARCH__)
-    int msb = __clz(cell);
-    int lsb = sizeof(unsigned int) * 8 - msb - 1;
-  #else
-    int lsb = __builtin_ctz(cell);
-  #endif
+__host__ __device__ int cell_to_digit(uint16_t cell)
+{
+// Get the index of the least-significant bit in this cell's value
+#if defined(__CUDA_ARCH__)
+  int msb = __clz(cell);
+  int lsb = sizeof(unsigned int) * 8 - msb - 1;
+#else
+  int lsb = __builtin_ctz(cell);
+#endif
 
   // Is there only one possible value for this cell? If so, return it.
   // Otherwise return zero.
@@ -231,9 +243,12 @@ __host__ __device__ int cell_to_digit(uint16_t cell) {
  * \param str     The input string that encodes the board
  * \returns       true if parsing succeeds, false otherwise
  */
-bool read_board(board_t* output, const char* str) {
-  for (int index = 0; index < BOARD_DIM * BOARD_DIM; index++) {
-    if (str[index] < '0' || str[index] > '9') return false;
+bool read_board(board_t *output, const char *str)
+{
+  for (int index = 0; index < BOARD_DIM * BOARD_DIM; index++)
+  {
+    if (str[index] < '0' || str[index] > '9')
+      return false;
 
     // Convert the character value to an equivalent integer
     int value = str[index] - '0';
@@ -251,19 +266,25 @@ bool read_board(board_t* output, const char* str) {
  *
  * \param board   The sudoku board to print
  */
-void print_board(board_t* board) {
-  for (int row = 0; row < BOARD_DIM; row++) {
+void print_board(board_t *board)
+{
+  for (int row = 0; row < BOARD_DIM; row++)
+  {
     // Print horizontal dividers
-    if (row != 0 && row % GROUP_DIM == 0) {
-      for (int col = 0; col < BOARD_DIM * 2 + BOARD_DIM / GROUP_DIM; col++) {
+    if (row != 0 && row % GROUP_DIM == 0)
+    {
+      for (int col = 0; col < BOARD_DIM * 2 + BOARD_DIM / GROUP_DIM; col++)
+      {
         printf("-");
       }
       printf("\n");
     }
 
-    for (int col = 0; col < BOARD_DIM; col++) {
+    for (int col = 0; col < BOARD_DIM; col++)
+    {
       // Print vertical dividers
-      if (col != 0 && col % GROUP_DIM == 0) printf("| ");
+      if (col != 0 && col % GROUP_DIM == 0)
+        printf("| ");
 
       // Compute the index of this cell in the board array
       int index = col + row * BOARD_DIM;
@@ -291,29 +312,37 @@ void print_board(board_t* board) {
  * \param solved_count  Output: A pointer to the count of solved boards.
  * \param error:count   Output: A pointer to the count of incorrect boards.
  */
-void check_solutions(board_t* boards,
-                     board_t* solutions,
+void check_solutions(board_t *boards,
+                     board_t *solutions,
                      size_t num_boards,
-                     size_t* solved_count,
-                     size_t* error_count) {
+                     size_t *solved_count,
+                     size_t *error_count)
+{
   // Loop over all the boards in this batch
-  for (int i = 0; i < num_boards; i++) {
+  for (int i = 0; i < num_boards; i++)
+  {
     // Does the board match the solution?
-    if (memcmp(&boards[i], &solutions[i], sizeof(board_t)) == 0) {
+    if (memcmp(&boards[i], &solutions[i], sizeof(board_t)) == 0)
+    {
       // Yes. Record a solved board
       (*solved_count)++;
-    } else {
+    }
+    else
+    {
       // No. Make sure the board doesn't have any constraints that rule out
       // values that are supposed to appear in the solution.
       bool valid = true;
-      for (int j = 0; j < BOARD_DIM * BOARD_DIM; j++) {
-        if ((boards[i].cells[j] & solutions[i].cells[j]) == 0) {
+      for (int j = 0; j < BOARD_DIM * BOARD_DIM; j++)
+      {
+        if ((boards[i].cells[j] & solutions[i].cells[j]) == 0)
+        {
           valid = false;
         }
       }
 
       // If the board contains an incorrect constraint, record an error
-      if (!valid) (*error_count)++;
+      if (!valid)
+        (*error_count)++;
     }
   }
 }
@@ -321,16 +350,19 @@ void check_solutions(board_t* boards,
 /**
  * Entry point for the program
  */
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   // Check arguments
-  if (argc != 2) {
+  if (argc != 2)
+  {
     fprintf(stderr, "Usage: %s <input file name>\n", argv[0]);
     exit(1);
   }
 
   // Try to open the input file
-  FILE* input = fopen(argv[1], "r");
-  if (input == NULL) {
+  FILE *input = fopen(argv[1], "r");
+  if (input == NULL)
+  {
     fprintf(stderr, "Failed to open input file %s.\n", argv[1]);
     perror(NULL);
     exit(2);
@@ -352,17 +384,20 @@ int main(int argc, char** argv) {
   size_t batch_count = 0;
 
   // Read the input file line-by-line
-  char* line = NULL;
+  char *line = NULL;
   size_t line_capacity = 0;
-  while (getline(&line, &line_capacity, input) > 0) {
+  while (getline(&line, &line_capacity, input) > 0)
+  {
     // Read in the starting board
-    if (!read_board(&boards[batch_count], line)) {
+    if (!read_board(&boards[batch_count], line))
+    {
       fprintf(stderr, "Skipping invalid board...\n");
       continue;
     }
 
     // Read in the solution board
-    if (!read_board(&solutions[batch_count], line + BOARD_DIM * BOARD_DIM + 1)) {
+    if (!read_board(&solutions[batch_count], line + BOARD_DIM * BOARD_DIM + 1))
+    {
       fprintf(stderr, "Skipping invalid board...\n");
       continue;
     }
@@ -374,7 +409,8 @@ int main(int argc, char** argv) {
     board_count++;
 
     // If we finished a batch, run the solver
-    if (batch_count == BATCH_SIZE) {
+    if (batch_count == BATCH_SIZE)
+    {
       size_t start_time = time_ms();
       solve_boards(boards, batch_count);
       solving_time += time_ms() - start_time;
@@ -387,7 +423,8 @@ int main(int argc, char** argv) {
   }
 
   // Check if there's an incomplete batch to solve
-  if (batch_count > 0) {
+  if (batch_count > 0)
+  {
     size_t start_time = time_ms();
     solve_boards(boards, batch_count);
     solving_time += time_ms() - start_time;
@@ -400,7 +437,8 @@ int main(int argc, char** argv) {
   double solving_rate = (double)solved_count / seconds;
 
   // Don't print nan when solver is not implemented
-  if (seconds < 0.01) solving_rate = 0;
+  if (seconds < 0.01)
+    solving_rate = 0;
 
   printf("Boards: %lu\n", board_count);
   printf("Boards Solved: %lu\n", solved_count);
